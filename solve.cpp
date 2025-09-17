27fe5b112615d119cef527c4274ff2528e29bbb9
vaibhav7024 <vjaiswal7024@gmail.com>
2025-09-17 21:52:46 +0530

    Create solve.cpp

---
+++ solve.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i=0;
+        while(isspace(s[i])) i++;
+
+        int sign=1;
+        if(s[i]=='-'){
+            sign=-1;
+            i++;
+        }else if(s[i]=='+'){
+            sign=1;
+            i++;
+        }
+
+        int num=0;
+        while(isdigit(s[i])){
+           int digit = s[i] - '0';
+
+        // Check for overflow before multiplying
+        if (num > INT_MAX / 10 || (num == INT_MAX / 10 && digit > INT_MAX % 10)) {
+            return sign == 1 ? INT_MAX : INT_MIN;
+        }
+        num=num*10+digit;
+            i++;
+        }
+
+        num*=sign;
+        return num;
+    }
+};
